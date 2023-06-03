0e62a368c793349b7442bef9f4c0e0b21e4202fa
abdellah nejmeddine <48094988+NejmeddineAbdellah@users.noreply.github.com>
2023-06-03 16:48:52 +0100

    Update RestauController.java

--- src/main/java/ma/restaurant/projet/rest/RestauController.java
+++ src/main/java/ma/restaurant/projet/rest/RestauController.java
@@ -11,12 +11,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import ma.restaurant.projet.entity.Restau;
 import ma.restaurant.projet.service.RestauService;
 
 @RestController
 @RequestMapping("api/restaus")
+@CrossOrigin("*")
 public class RestauController {
 
 	@Autowired
